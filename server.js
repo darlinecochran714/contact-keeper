6ae53f9cb3a1f2e8b5ebd558a44f08b15e3beb1c
darlinecochran714 <arlinecochrand@gmail.com>
2020-08-16 17:18:58 -0400

    added in config folder. We are successfully connected to MongoDB

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 // starting our express server 
 const express = require('express');
+// bringing in the DB
+const connectDB = require('./config/db')
+
+// connecting database
+connectDB();
 
 // initializing express in a variable called 'app
 const app = express();
