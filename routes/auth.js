ac6c78403444c8c924e70ea2d8a0fe7e03a29343
darlinecochran714 <arlinecochrand@gmail.com>
2020-08-16 16:41:17 -0400

    set up basic auth route

--- routes/auth.js
+++ routes/auth.js
@@ -1,1 +1,15 @@
 // login, authincation and a route to check in logged in user
+
+const express = require('express');
+const router = express.Router();
+
+// this is a get request, getting something to the server
+// @route       GET api/auth
+// @desc        get logged in user
+// @access      private
+router.get('/', (req, res) => {
+    res.send('get logged in user')
+});
+
+// dont forget to export or else the router wont work
+module.exports = router;
