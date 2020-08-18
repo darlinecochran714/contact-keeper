ec837b3e77d9253152bde26edac4fd1390752484
darlinecochran714 <arlinecochrand@gmail.com>
2020-08-17 20:07:41 -0400

    added in contact, update and delete routes. tested with postamn and they are working

--- routes/contacts.js
+++ routes/contacts.js
@@ -23,28 +23,107 @@ router.get('/', auth, async (req, res) => {
     }
 });
 
-
 // @route       Post api/contacts
 // @desc        add new contact
 // @access      private
-router.post('/', (req, res) => {
-    res.send('add new contact')
+router.post('/', [ auth, [
+    // using both our auth middleware and express validator middlewear
+    check('name', "name is needed").not().isEmpty()
+]], async (req, res) => {
+     // this is for routes that accept data and needs validation
+     const errors = validationResult(req);
+
+     // if not errors is empty then return a 400 status and sends json data to an error array
+     if(!errors.isEmpty()){
+         return res.status(400).json({ errors: errors.array() })
+    }
+
+    const { name, email, phone, type } = req.body;
+
+    try{
+        const newContact = new Contact({
+            name,
+            email, 
+            phone,
+            type,
+            user: req.user.id
+        });
+
+        // saving to the DB
+        const contact = await newContact.save();
+
+        res.json(contact);
+    } catch(err) {
+        console.error(err.message);
+        res.status(500).send('service error')
+    }
 });
 
 
 // @route       Put api/contacts/:id
 // @desc        update contact
 // @access      private
-router.put('/:is', (req, res) => {
-    res.send('update contact')
+router.put('/:id', auth, async (req, res) => {
+    const { name, email, phone, type } = req.body;
+
+    // building a contact object
+    const contactFields = {};
+    // checking to see if the fields are submitted
+    if(name) contactFields.name = name;
+    if(email) contactFields.email = email;
+    if(phone) contactFields.phone = phone;
+    if(type) contactFields.type = type;
+
+    try{
+        // finds contact by id
+        let contact = await Contact.findById(req.params.id);
+
+        // if id is not found, send an error
+        if(!contact) return res.status(404).json({ msg: "Contact not found "});
+
+        // makes sure the user owns the contact
+        // if logged in user is not equal to the contact user
+        if(contact.user.toString() !== req.user.id) {
+            // returns unauthorized
+            return res.status(401).json({ msg: 'not authorized'})
+        }
+
+        // takes in the contact ID and passes in the  the body from contact fields and if it dosent exist, then create.
+        contact = await Contact.findByIdAndUpdate(req.params.id, { $set: contactFields }, { new: true });
+
+        res.json(contact)
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('service error')
+    }
 });
 
 
 // @route       DELETE api/contact:id
 // @desc        delete contact
 // @access      private
-router.delete('/', (req, res) => {
-    res.send('delete a contact')
+router.delete('/:id', auth, async (req, res) => {
+    try{
+        // finds contact by id
+        let contact = await Contact.findById(req.params.id);
+
+        // if id is not found, send an error
+        if(!contact) return res.status(404).json({ msg: "Contact not found "});
+
+        // makes sure the user owns the contact
+        // if logged in user is not equal to the contact user
+        if(contact.user.toString() !== req.user.id) {
+            // returns unauthorized
+            return res.status(401).json({ msg: 'not authorized'})
+        }
+
+        await Contact.findByIdAndRemove(req.params.id);
+
+        res.json({ msg: 'contact removed'})
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('service error')
+    }
 });
 
 // dont forget to export or else the router wont work
