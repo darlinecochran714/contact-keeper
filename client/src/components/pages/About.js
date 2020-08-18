57f97fba8be0aa2df07a4e9a495afdc3a217daeb
darlinecochran714 <arlinecochrand@gmail.com>
2020-08-18 07:00:42 -0400

    added functionality to lets react and the derver run at the same time. Set up nav bar and basic react router set up

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            <h1>V1.0.0</h1>
+            <p className = 'my-1'>
+                an App for keeping contacts
+            </p>
+        </div>
+    )
+}
+
+export default About
